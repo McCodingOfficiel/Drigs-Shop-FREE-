e2a44618945665951fdd7c3f7328d8062196a299
McCodingOfficiel <mctow.officiel@gmail.com>
2024-08-12 23:17:27 +0200

    Add files via upload

---
+++ CBD_Shop/item.sql
@@ -0,0 +1,9 @@
+INSERT INTO `items` (name, label) VALUES 
+	('coke', 'Coke'),
+	('weed', "Weed"),
+	('crack', 'Crack'),
+    ('opium', 'Opium'),
+	('meth', "Meth"),
+    ('ketamine', 'Ketamine'),
+    ('ecstasy', 'Ecstasy')
+;
